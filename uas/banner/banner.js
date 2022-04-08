54cf3399a76de3749e314f85a601613686beb7c5
amlovey <amlovey@qq.com>
2022-04-08 12:21:13 +0800

    Update banner.js

--- uas/banner/banner.js
+++ uas/banner/banner.js
@@ -1,5 +1,5 @@
 (function (window) {
-    window.UASLink = "https://assetstore.unity.com/?on_sale=true&orderBy=1&rows=96&aid=1011lGoJ";
+    window.UASLink = "https://assetstore.unity.com/?flashdeals=true&aid=1011lGoJ";
     window.UASBannerImg = "https://www.amlovey.com/uas/banner/banner.jpg";
     window.UASBannerEnable = true;
 
